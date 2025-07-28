39f0f3318ace945de63619f728c367f0df929da1
Komal Arora <komal.arora@quincus.com>
2025-07-28 14:32:08 +0530

    Red: input '1' returns 1

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe StringCalculator do
     it "returns 0 for empty string" do
       expect(StringCalculator.add("")).to eq(0)
     end
+
+    it "returns 1 for input '1'" do
+        expect(StringCalculator.add("1")).to eq(1)
+    end 
   end
 end
