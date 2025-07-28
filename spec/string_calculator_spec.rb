585e9751589341636cbcaa9c279b9e60bf8d7137
Komal Arora <komal.arora@quincus.com>
2025-07-28 14:38:48 +0530

    Red: test for newline separator

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -17,5 +17,9 @@ RSpec.describe StringCalculator do
     it "returns sum of multiple numbers" do
         expect(StringCalculator.add("1,2,3,4")).to eq(10)
     end
+
+    it "supports newline as separator" do
+        expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
   end
 end
